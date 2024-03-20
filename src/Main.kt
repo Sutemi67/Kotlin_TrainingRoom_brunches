59cadc8d0049fb4ddfc46c140be184de8873997e
sutemi67 <sutemi67@gmail.com>
2024-03-20 23:41:18 +0700

    first day

--- src/Main.kt
+++ src/Main.kt
@@ -1,30 +1,75 @@
 import java.util.*
 
 class CoffeeMachine {
+    private var water: Int = 0
+    private var milk: Int = 0
+    private var beans: Int = 0
+
+    enum class Types(val water: Int, val milk: Int, val beans: Int) {
+        ESPRESSO(60, 0, 10),
+        AMERICANO(120, 0, 10),
+        CAPPUCCINO(120, 20, 60),
+        LATTE(240, 20, 120)
+    }
+
     private var scanner = Scanner(System.`in`)
 
-    fun start(water: Int, milk: Int, beans: Int) {
+    fun start() {
         println("Кофемашина готова к работе")
-        var inputWords = scanner.nextLine()
-        var inputWordsLowercase = inputWords.lowercase()
+
         while (true) {
             println("Введите команду:")
-            when (inputWordsLowercase) {
+            val inputCoffee: String
+            val inputWords = scanner.nextLine().lowercase()
+
+            when (inputWords) {
                 "выключить" -> {
                     println("До свидания!")
                     break
                 }
-                "наполнить"->{
 
+                "наполнить" -> {
+                    water = 2000
+                    milk = 1000
+                    beans = 500
+                    println("Ингридиенты пополнены")
                 }
-                "статус"->{
 
+                "статус" -> {
+                    println("Ингридиентов осталось: \n$water мл воды\n$milk мл молока\n$beans гр кофе")
                 }
-                "кофе"->{
 
+                "кофе" -> {
+                    println("Введите название напитка или \"назад\" для возврата в главное меню")
+                    inputCoffee = scanner.nextLine().lowercase()
+                    when (inputCoffee) {
+                        "латте" -> {
+                            if (water >= Types.LATTE.water && milk >= Types.LATTE.milk && beans >= Types.LATTE.beans) {
+                                water -= Types.LATTE.water
+                                milk -= Types.LATTE.milk
+                                beans -= Types.LATTE.beans
+                                println("${Types.LATTE} готов")
+                            } else {
+                                if (water < Types.LATTE.water) {
+                                    println("Недостаточно воды!")
+                                } else if (milk < Types.LATTE.milk) {
+                                    println("Недостаточно молока!")
+                                } else {
+                                    println("Недостаточно кофе!")
+                                }
+                            }
+                        }
+                    }
+                }
+                else -> {
+                    println("Неверная команда.")
                 }
             }
         }
     }
 }
 
+fun main() {
+    val coffee = CoffeeMachine()
+    coffee.start()
+}
