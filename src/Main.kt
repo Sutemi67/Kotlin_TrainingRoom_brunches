4ca7a37a85989d152261c6c8d91dcf501e6562f9
sutemi67 <sutemi67@gmail.com>
2024-03-28 15:37:49 +0700

    initial

--- src/Main.kt
+++ src/Main.kt
@@ -1,128 +1,45 @@
-import java.util.*
-
-class CoffeeMachine {
-    private var water: Int = 0
-    private var milk: Int = 0
-    private var beans: Int = 0
-
-    enum class Types(val water: Int, val milk: Int, val beans: Int, val coffeeName:String) {
-        ESPRESSO(60, 0, 10, "эспрессо"),
-        AMERICANO(120, 0, 10, "американо"),
-        CAPPUCCINO(120, 60, 20, "капучино"),
-        LATTE(240, 120, 20, "латте")
-    }
-
-    private var scanner = Scanner(System.`in`)
-
-    fun start() {
-        println("Кофемашина готова к работе")
-
-        while (true) {
-            println("Введите команду:")
-            val inputCoffee: String
-            val inputWords = scanner.nextLine().lowercase()
-
-            when (inputWords) {
-                "выключить" -> {
-                    println("До свидания!")
-                    break
-                }
-
-                "наполнить" -> {
-                    water = 2000
-                    milk = 1000
-                    beans = 500
-                    println("Ингридиенты пополнены")
-                }
-
-                "статус" -> {
-                    println("Ингридиентов осталось: \n$water мл воды\n$milk мл молока\n$beans гр кофе")
-                }
-
-                "кофе" -> {
-                    println("Введите название напитка или \"назад\" для возврата в главное меню")
-                    inputCoffee = scanner.nextLine().lowercase()
-                    when (inputCoffee) {
-                        "латте" -> {
-                            if (water >= Types.LATTE.water && milk >= Types.LATTE.milk && beans >= Types.LATTE.beans) {
-                                water -= Types.LATTE.water
-                                milk -= Types.LATTE.milk
-                                beans -= Types.LATTE.beans
-                                println("${Types.LATTE.coffeeName} готов")
-                            } else {
-                                if (water < Types.LATTE.water) {
-                                    println("Недостаточно воды!")
-                                } else if (milk < Types.LATTE.milk) {
-                                    println("Недостаточно молока!")
-                                } else {
-                                    println("Недостаточно кофе!")
-                                }
-                            }
-                        }
-
-                        "эспрессо" -> {
-                            if (water >= Types.ESPRESSO.water && milk >= Types.ESPRESSO.milk && beans >= Types.ESPRESSO.beans) {
-                                water -= Types.ESPRESSO.water
-                                milk -= Types.ESPRESSO.milk
-                                beans -= Types.ESPRESSO.beans
-                                println("${Types.ESPRESSO.coffeeName} готов")
-                            } else {
-                                if (water < Types.ESPRESSO.water) {
-                                    println("Недостаточно воды!")
-                                } else if (milk < Types.ESPRESSO.milk) {
-                                    println("Недостаточно молока!")
-                                } else {
-                                    println("Недостаточно кофе!")
-                                }
-                            }
-                        }
-
-                        "американо" -> {
-                            if (water >= Types.AMERICANO.water && milk >= Types.AMERICANO.milk && beans >= Types.AMERICANO.beans) {
-                                water -= Types.AMERICANO.water
-                                milk -= Types.AMERICANO.milk
-                                beans -= Types.AMERICANO.beans
-                                println("${Types.AMERICANO.coffeeName} готов")
-                            } else {
-                                if (water < Types.AMERICANO.water) {
-                                    println("Недостаточно воды!")
-                                } else if (milk < Types.AMERICANO.milk) {
-                                    println("Недостаточно молока!")
-                                } else {
-                                    println("Недостаточно кофе!")
-                                }
-                            }
-                        }
-
-                        "капучино" -> {
-                            if (water >= Types.CAPPUCCINO.water && milk >= Types.CAPPUCCINO.milk && beans >= Types.CAPPUCCINO.beans) {
-                                water -= Types.CAPPUCCINO.water
-                                milk -= Types.CAPPUCCINO.milk
-                                beans -= Types.CAPPUCCINO.beans
-                                println("${Types.CAPPUCCINO.coffeeName} готов")
-                            } else {
-                                if (water < Types.CAPPUCCINO.water) {
-                                    println("Недостаточно воды!")
-                                } else if (milk < Types.CAPPUCCINO.milk) {
-                                    println("Недостаточно молока!")
-                                } else {
-                                    println("Недостаточно кофе!")
-                                }
-                            }
-                        }
-
-                        "назад" -> continue
-                        else -> {
-                            println("Рецепт не найден!")
-                            continue
-                        }
-                    }
-                }
-
-                else -> {
-                    println("Неверная команда.")
-                }
-            }
-        }
-    }
+enum class GroupRole(val title: String) {
+    VOCAL("Вокалист"),
+    BASS("Бассист"),
+    SOLO("Соло-гитарист"),
+    DRUMS("Барабанщик")
 }
+
+abstract class RockMusician {
+
+    abstract val name: String
+    abstract val role: GroupRole
+}
+
+interface CanSing {
+
+    fun sing()
+}
+
+interface CanPlayGuitar {
+
+    fun playGuitar()
+}
+
+interface CanPlayDrum {
+
+    fun playDrum()
+}
+
+// унаследуйте данный класс от абстрактного класса RockMusician
+// имя музыканта должно был переопределено в конструкторе
+// его роль должна быть задана внутри класса как GroupRole.VOCAL
+// вокалист должен уметь петь - реализуйте интерфейс CanSing. Метод sing() должен выводить текст: "We will, we will rock you"
+class Vocalist
+
+// унаследуйте данный класс от абстрактного класса RockMusician
+// имя музыканта должно был переопределено в конструкторе
+// поскольку и басс, и соло гитаристы являются гитаристами - их роль, как и имя, передаётся при создании в конструкторе.
+// гитаристы должны уметь играть на гитаре. Реализуйте интерфейс CanPlayGuitar. Метод playGuitar() выводит текст "Играю на гитаре"
+class Guitarist
+
+// унаследуйте данный класс от абстрактного класса RockMusician
+// имя музыканта должно был переопределено в конструкторе
+// его роль должна быть задана внутри класса как GroupRole.DRUMS
+// барабанщик играет на ударных. Интерфейс CanPlayDrum поможет в этом, реализуйте его. Метод playDrum() выводит текст "Пам парам пам пам, я играю на барабанах"
+class Drummer
