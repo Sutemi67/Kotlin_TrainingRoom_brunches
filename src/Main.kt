0a1187b481776d8c6a2241091c063565c2655682
sutemi67 <sutemi67@gmail.com>
2024-04-02 20:16:27 +0700

    2 part, saving

--- src/Main.kt
+++ src/Main.kt
@@ -2,54 +2,49 @@ import kotlin.random.Random
 
 class Lotto {
 
-    private val persons: MutableList<Person> = mutableListOf()
+    val persons: MutableList<Person> = mutableListOf()
     val thrownNumbers: MutableSet<Int> = mutableSetOf()
 
     fun addPerson(person: Person) {
         persons.add(person)
     }
 
-
     fun start() {
-        if (persons.size > 1) {
+        if (persons.size <= 1) {
+            println("Перед началом игры необходимо добавить хотя бы двух игроков")
+            return
+        } else {
+            val personIterator = persons.iterator()
             while (true) {
-                val thrownNumber = generateUniqueNumber()
+                val thrownNumber = Random.nextInt(1, 100)
                 thrownNumbers.add(thrownNumber)
-                println("Выброшенное число $thrownNumber")
 
-                for (person in persons) {
-                    val card = person.card
-                    for (line in card.numbers.keys) {
-                        if (thrownNumber in card.numbers[line]!!) {
-                            card.numbers[line]!!.remove(thrownNumber)
+                for (key in 0..personIterator.next().card.numbers.keys.size) {
+
+                    while (personIterator.hasNext()) {
+                        val cardKeys = personIterator.next().card.numbers[key]
+                        val mapIterator = cardKeys!!.iterator()
+
+                        while (mapIterator.hasNext()) {
+                            if (mapIterator.next() == thrownNumber) {
+                                mapIterator.remove()
+                            }
+                        }
+                        if (cardKeys.size == 0) {
+                            println("Победитель: ${personIterator.next()}!!!")
+                            return
                         }
                     }
                 }
-
-                val winners = persons.filter { person ->
-                    person.card.numbers.values.all { it.isEmpty() }
-                }
-
-                if (winners.isNotEmpty()) {
-                    for (winner in winners) {
-                        println("Победитель: ${winner.name}!!!")
-                    }
-                    break
-                }
             }
-        } else {
-            println("Перед началом игры необходимо добавить хотя бы двух игроков")
         }
     }
-
-    private fun generateUniqueNumber(): Int {
-        var number: Int
-        do {
-            number = Random.nextInt(1,100)
-        } while (number in thrownNumbers)
-        return number
-    }
 }
+// Достать номер. Номер может быть в диапазоне от 1 до 99 включительно
+// после каждого выброшенного числа удалять это число из карточек всех игроков, если такое число имеется
+// выбрасывать новые числа до тех пор, пока не определится победитель
+// побеждает тот, у кого в одном из рядов нет больше чисел. Победителей может быть более одного
+// после того как появляется победитель, для каждого победителя вывести текст "Победитель: [имя_победителя]!!!"
 
 
 class Card(val numbers: Map<Int, MutableSet<Int>>)
