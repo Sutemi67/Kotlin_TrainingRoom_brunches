779c4282d0ab5919cdac5e9c003bd13e58e17d31
sutemi67 <sutemi67@gmail.com>
2024-04-04 14:40:38 +0700

    working

--- src/Main.kt
+++ src/Main.kt
@@ -1,47 +1,58 @@
-import kotlin.random.Random
+class Person(val name: String, val surname: String, val thirdName: String)
 
+// реализуйте свойство firstSymbol для класса String
+// "Белка".firstSymbol -> Результат 'Б'
+// "автомобиль".firstSymbol -> Результат 'а'
+val String.firstSymbol: Char
+    get() {
+        val firstLetter = this.toCharArray().first()
+        return firstLetter
+    }
+
+// реализуйте свойство firstDigit для класса Int
+// val a = 435
+// a.firstDigit -> Результат 4
+val Int.firstDigit: Int
+    get() {
+        var num = this
+        while (num >= 10) {
+            num /= 10
+        }
+        return num
+    }
+
+
+// реализуйте свойство fio для класса Person
+// Person("Андрей", "Стрельцов", "Александрович") -> Результат "Стрельцов Андрей Александрович"
+val Person.fio: String
+    get() {
+        return "${this.surname} ${this.name} ${this.thirdName}"
+    }
+
+// реализуйте свойство biggestDigit для класса Int
+// val a = 435
+// a.biggestDigit -> Результат 5
+val Int.biggestDigit: Int
+    get() {
+        var most = 0
+        var remaining = this
+        var lastDigit = 0
+        while (remaining >= 10) {
+            lastDigit = remaining % 10
+            remaining /= 10
+            if (lastDigit > most)
+                most = lastDigit
+
+        }
+        return most
+    }
 
 fun main() {
-    val catDownloader = CatDownloader()
-
-    catDownloader.downloadCat(
-        onSuccess = { cat -> println("Кот ${cat.name} загружен успешно") },
-        onError = { error -> println(error) },
-        onStart = { println("Загрузчик Котиков начинает работу") },
-        allowErrors = false
-    )
+    val str = "Привет че делаешь"
+    val int = 32352
+    val person = Person("Серега", "Бойков", "Сергеевич")
+    println(str.firstSymbol)
+    println(int.firstDigit)
+    println(person.fio)
+    println(int.biggestDigit)
 }
-
-class CatDownloader {
-
-    fun downloadCat(
-        onSuccess: (Cat) -> Unit,
-        onError: (String) -> Unit,
-        onStart: () -> Unit,
-        allowErrors: Boolean
-    ) {
-        onStart()
-        if (!allowErrors) {
-            while (true) {
-                val cat: Cat? = getCatFromInternet()
-                if (cat != null) {
-                    onSuccess.invoke(cat)
-                    break
-                }
-            }
-        } else {
-            val cat: Cat? = getCatFromInternet()
-            if (cat != null) {
-                onSuccess.invoke(cat)
-            } else {
-                onError("Упс, что-то пошло не так при загрузке котеек.")
-            }
-        }
-    }
-
-    private fun getCatFromInternet(): Cat? {
-        return if (Random.nextBoolean()) Cat("Борис") else null
-    }
-}
-
-class Cat(val name: String)
